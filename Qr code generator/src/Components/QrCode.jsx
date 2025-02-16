2a3b9526f7fa688df0408036371351b51cf494b3
arunkumar <arunganesh7070@gmail.com>
2025-02-16 21:35:03 +0530

    add css

--- Qr code generator/src/Components/QrCode.jsx
+++ Qr code generator/src/Components/QrCode.jsx
@@ -4,8 +4,21 @@ import React from 'react'
 
 export const QrCode = () => {
   return (
-    <div>QrCode
-        <p>hhhhh</p>
+    <div className='app_container'>
+      <h1>OR CODE GENERATOR</h1>
+      <img src='/' className='qr_code_image'></img>
+      <div>
+        <label htmlFor='dataInput' className='input_label'>
+          Data for QR code
+        </label>
+        <input type='text' id='dataInput'/>
+         <label htmlFor='sizeInput' className='input_label'>
+          Enter the size of the OR code
+        </label>
+        <input type='text' id='sizeInput'/>
+        <button className='generate_btn'> Generate QR code</button>
+        <button className='download_btn'>Download QR code</button>
+      </div>
     </div>
   )
 }
