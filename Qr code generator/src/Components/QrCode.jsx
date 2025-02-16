9a9d8a623dd271fdcbb6ad7748e536e56402630a
arunkumar <arunganesh7070@gmail.com>
2025-02-16 21:54:34 +0530

    add the state

--- Qr code generator/src/Components/QrCode.jsx
+++ Qr code generator/src/Components/QrCode.jsx
@@ -1,12 +1,20 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export const QrCode = () => {
+  const [img,setImg]=useState("qrcode.png")
+  function generateQrCode(){
+    setImg("qrcode.png")
+    
+  }
+  function downloadQrCode(){
+    
+  }
   return (
     <div className='app_container'>
       <h1>OR CODE GENERATOR</h1>
-      <img src='/' className='qr_code_image'></img>
+      <img src={img} className='qr_code_image'></img>
       <div>
         <label htmlFor='dataInput' className='input_label'>
           Data for QR code
@@ -16,9 +24,10 @@ export const QrCode = () => {
           Enter the size of the OR code
         </label>
         <input type='text' id='sizeInput'/>
-        <button className='generate_btn'> Generate QR code</button>
-        <button className='download_btn'>Download QR code</button>
+        <button className='generate_btn' onClick={() => { generateQrCode() }}> Generate QR code</button>
+        <button className='download_btn' onClick={() => { downloadQrCode() }}>Download QR code</button>
       </div>
+      <p className='footer'> Designed by Arunkumar</p>
     </div>
   )
 }
